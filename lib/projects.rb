bd359726207124adf5ab7c7b1ba866d905a9474e
Student <example@example.com>
2020-01-10 10:52:34 -0800

    add volunteer_id in initialize

--- lib/projects.rb
+++ lib/projects.rb
@@ -1,17 +1,17 @@
 class Project
-  attr_accesor :name, :id
+  attr_accesor :name, :id, :volunteer_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name, nil)
     @id = attributes.fetch(:id, nil)
+    @volunteer_id = attributes.fetch(:volunteer_id, nil)
+  end
+
+  def self.all
 
   end
 
-  def method_name
-
-  end
-
-  def method_name
+  def self.search
 
   end
 
