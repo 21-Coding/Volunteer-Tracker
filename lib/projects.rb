3075f5b04bd8edd1b4ef633eeded91efd543457c
Student <example@example.com>
2020-01-10 09:41:55 -0800

    add projects class with wireframing for methods

---
+++ lib/projects.rb
@@ -0,0 +1,22 @@
+class Projects
+  attr_accesor :name, :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name, nil)
+    @id = attributes.fetch(:id, nil)
+
+  end
+
+  def method_name
+
+  end
+
+  def method_name
+
+  end
+  
+  def method_name
+
+  end
+
+end
