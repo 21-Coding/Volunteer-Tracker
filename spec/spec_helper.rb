2d6dd1277ece97e2c4b61006703c70965e8dc8fd
Student <example@example.com>
2020-01-13 06:39:15 -0800

    add another attempt to connect database to project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'projects'
 require 'pry'
 
 
-DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+DB = PG.connect({:dbname => 'volunteer_tracker'})
 
 Rspec.configure do |confignewton|
   confignewton.after(:each) do
