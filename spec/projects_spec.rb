9764e911ee2de12c4923cd595efa515290e910ae
DominiqueYoumans <ddyoumans@icloud.com>
2020-01-12 23:07:53 -0800

    add wireframing for future tests

--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 require 'rspec'
+require 'project'
+
 
 describe '#Project' do
-
   describe('#==') do
-    it ("") do
-
+    it("is the same project if it has the same attributes as another project") do
+      project = Project.new({:name => "The Philadelphia Experiment", :id => nil})
+      project.save()
+      project2 = Project.new({:name => "The Philadelphia Experiment", :id => nil})
+      project.save()
+      expect(project).to(eq(project2))
     end
   end
 end
